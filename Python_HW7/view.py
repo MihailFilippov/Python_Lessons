4b06d1961ca2e8bcce74f8c69e132f6a26402c6f
Michail <miha.filippov.10@mail.ru>
2022-10-21 17:08:11 +0800

    HW7

---
+++ Python_HW7/view.py
@@ -0,0 +1,21 @@
+def get_name():
+    with open('Python_Lessons/Python_HW7/file.txt', 'r',encoding="utf-8") as f:
+        for line in f.readlines()[:1]:
+            return (line.rstrip('\n')) 
+        
+
+def get_number():
+    with open('file.txt', 'r', encoding="utf-8") as f:
+        for line in f.readlines()[1:2]:
+            return (line.rstrip('\n'))
+
+def what_to_do():
+    with open('file.txt', 'r', encoding="utf-8") as f:
+        for line in f.readlines()[2:]:
+            return (line.rstrip('\n'))
+
+
+def export_book(book: dict):
+    with open('newfile.txt','a',encoding="utf-8") as newfile:
+        newfile.writelines(f"{book}")
+        newfile.writelines('\n')
